5de23265cc6591f7f384ef09789fd43e2024f51e
Alejandro Fuentes <alesetneuf@gmail.com>
2019-06-17 19:18:53 +0100

    done

--- suma.test.js
+++ suma.test.js
@@ -2,9 +2,6 @@ const { sumar } = require("./index.js");
 
 describe("sumar", () =>{
     it("comprobar suma", () =>{
-    
-
-        const resultado = sumar(num1,num2);
 
         expect(sumar(3,2)).toEqual(5);
     })
