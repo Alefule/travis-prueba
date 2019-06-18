7cc1ed0a1289f568e083dad2c75f544f7e89697e
Alejandro Fuentes <alesetneuf@gmail.com>
2019-06-18 20:46:09 +0100

    Deploy digital

---
+++ scripts/deploy.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+COMMAND="cd ${REMOTE_PATH} && git pull"
+ssh -o StrictHostKeyChecking=no -i deploy-travis -v ${REMOTE_PORT} ${REMOTE_USER}@${REMOTE_HOST} ${COMMAND}  
---
+++ scripts/install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+openssl aes-256-cbc -K $encrypted_bb536893562e_key -iv $encrypted_bb536893562e_iv -in deploy_travis.enc -out deploy_travis -d
+eval "$(ssh-agent -s)"
+chmod 600 deploy-travis 
+ssh-add deploy-travis
