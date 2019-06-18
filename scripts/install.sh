6ff662844998568541bc2227f77e59d164278854
Alejandro Fuentes <alesetneuf@gmail.com>
2019-06-18 20:52:49 +0100

    Deploy digital

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,5 +2,5 @@
 
 openssl aes-256-cbc -K $encrypted_bb536893562e_key -iv $encrypted_bb536893562e_iv -in deploy_travis.enc -out deploy_travis -d
 eval "$(ssh-agent -s)"
-chmod 600 deploy-travis 
-ssh-add deploy-travis
+chmod 600 deploy_travis 
+ssh-add deploy_travis
